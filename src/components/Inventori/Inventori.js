f72e9e3b41a967dcbed37fbabe36b75da81d0be9
masudrana-phero <masudcse911@gmail.com>
2021-10-01 22:07:18 +0600

    added the inventori page

---
+++ src/components/Inventori/Inventori.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventori = () => {
+    return (
+        <div>
+            <h1>This is Inventori</h1>
+        </div>
+    );
+};
+
+export default Inventori;
